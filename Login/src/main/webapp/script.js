3d8545f3380db3c8371238be1de90d2794ea1a8d
Anthony Lapham <lapham.2@wright.edu>
2021-11-30 18:36:49 -0500

    fix config

--- Login/src/main/webapp/script.js
+++ Login/src/main/webapp/script.js
@@ -2,11 +2,15 @@ $(document).ready(function(){
 	var apiURL = "https://holidayapi.com/v1/holidays?pretty&country=US&year=2020&key=" + api_Key;
 	var api_Key = config.API_KEY;
 	
+	$("#register-form").hide();
+	
 	 $.getJSON(apiURL, function(data) {
       console.log(data);
-	  if(data.holidays.name = "Christmas Day"){
+	  if(data.holidays.weekday.date.name = "Tuesday"){
 		document.body.style.backgroundColor = rgb(20, 65, 37);
 	}
+	
+
       
      
   });
