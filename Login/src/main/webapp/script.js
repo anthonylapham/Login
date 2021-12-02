99253541f21d964f6a888404a94a1985d2b79d73
Anthony Lapham <lapham.2@wright.edu>
2021-12-02 12:14:03 -0500

    trying to change background.

--- Login/src/main/webapp/script.js
+++ Login/src/main/webapp/script.js
@@ -1,12 +1,13 @@
 $(document).ready(function(){
-	var apiURL = "https://holidayapi.com/v1/holidays?pretty&country=US&year=2020&key=" + api_Key;
 	var api_Key = config.API_KEY;
+	var apiURL = "https://holidayapi.com/v1/holidays?pretty&country=US&year=2020&key=" + api_Key;
+	
 	
 	$("#register-form").hide();
 	
 	 $.getJSON(apiURL, function(data) {
       console.log(data);
-	  if(data.holidays.weekday.date.name = "Tuesday"){
+	  if(data.holidays.weekday.date.name = "Thursday"){
 		document.body.style.backgroundColor = rgb(20, 65, 37);
 	}
 	
